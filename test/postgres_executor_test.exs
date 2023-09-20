dd3c6f1ef7be0f6ccd87cb8d50bf994f1c312b2e
Michael Bianco <iloveitaly@gmail.com>
2023-09-20 09:51:29 -0600

    test: expanded test cases

--- test/postgres_executor_test.exs
+++ test/postgres_executor_test.exs
@@ -2,11 +2,29 @@ defmodule PostgresExecutorTest do
   use ExUnit.Case
   doctest PostgresExecutor
 
-  test "greets the world" do
+  test "fails on bad connection string" do
+    assert {:error, _} =
+             PostgresExecutor.execute_sql_file(
+               "postgres://postgres:bad@localhost:5432/postgres_test",
+               "test/audit_trigger.sql"
+             )
+  end
+
+  test "fails on bad file reference" do
+    assert {:error, _} =
+             PostgresExecutor.execute_sql_file(
+               "postgres://postgres:postgres@localhost:5432/postgres_test",
+               "test/bad.sql"
+             )
+  end
+
+  test "executes complex sql" do
     assert {:ok, output} =
              PostgresExecutor.execute_sql_file(
                "postgres://postgres:postgres@localhost:5432/postgres_test",
                "test/audit_trigger.sql"
              )
+
+    IO.puts(output)
   end
 end
